21c79a6aade372793ab58672349e92d82c92b65a
yirklum <yirklum@aol.com>
2018-05-17 19:38:05 -0400

    Add files via upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,125 @@
+// Create an array of words
+
+var wordList =
+    ["SPRINGFIELD", "HOMER", "KRUSTY", "BART", "BORT", "SKINNER", "SMITHERS", "LISA", "MAGGIE",
+        "MARGE", "MILLHOUSE", "DOH", "ISOTOPES", "WIGGUM", "NELSON", "FLANDERS", "QUIMBY", "KRABAPPEL"
+    ];
+
+// Declare variables
+
+const guessesAllowed = 8;
+var usedLetters = [];
+var currentWordIndex;
+var currentWord = [];
+var guessesRemaining = 0;       
+var gameOver = false;
+var totalWins = 0;
+
+// Add sound
+
+var keySound = new Audio('./assets/sounds/typewriter-key.wav');
+var winSound = new Audio('./assets/sounds/you-win.wav');
+var loseSound = new Audio('./assets/sounds/you-lose.wav');
+
+// Set up the game, pick a new word
+
+function resetGame() {
+    guessesRemaining = guessesAllowed;
+    currentWordIndex = Math.floor(Math.random() * (wordList.length));
+    usedLetters = [];
+    currentWord = [];
+
+    for (var i = 0; i < wordList[currentWordIndex].length; i++) {
+        currentWord.push("_");
+    }   
+
+    // document.getElementById("pressKeyToStart").style.cssText= "display: none";
+    document.getElementById("gameover-image").style.cssText = "display: none";
+    document.getElementById("youwin-image").style.cssText = "display: none";
+
+    updateDisplay();
+};
+
+// Update stats
+
+function updateDisplay() {
+
+    document.getElementById("totalWins").innerText = totalWins;
+    var currentWordText = "";
+    for (var i = 0; i < currentWord.length; i++) {
+        currentWordText += currentWord[i];
+    }
+
+    document.getElementById("currentWord").innerText = currentWordText;
+    document.getElementById("guessesRemaining").innerText = guessesRemaining;
+    document.getElementById("usedLetters").innerText = usedLetters;
+};
+
+// Check if a guessed letter is in the word
+
+function evaluateGuess(letter) {
+    var positions = [];
+    for (var i = 0; i < wordList[currentWordIndex].length; i++) {
+        if(wordList[currentWordIndex][i] === letter) {
+            positions.push(i);
+        }
+    }
+        if (positions.length <= 0) {
+        guessesRemaining--;
+        } 
+        else {
+        for(var i = 0; i < positions.length; i++) {
+            currentWord[positions[i]] = letter;
+        }
+    }
+};
+
+// See if player has won
+
+function checkWin() {
+    if(currentWord.indexOf("_") === -1) {
+        document.getElementById("youwin-image").style.cssText = "display: block";
+        document.getElementById("pressKeyToStart").style.cssText= "display: block";
+        totalWins++;
+        winSound.play();
+        gameOver = true;
+    }
+};
+
+// See if player has lost
+
+function checkLoss() {
+    if(guessesRemaining <= 0) {
+        loseSound.play();
+        document.getElementById("gameover-image").style.cssText = "display: block";
+        document.getElementById("pressKeyToStart").style.cssText = "display:block";
+        gameOver = true;
+    }
+}
+
+// Note guessed letters
+
+function makeGuess(letter) {
+    if (guessesRemaining > 0) {
+        if (usedLetters.indexOf(letter) === -1) {
+            usedLetters.push(letter);
+            evaluateGuess(letter);
+        }
+    }  
+};
+
+// Event listener
+document.onkeydown = function(event) {
+    if(gameOver) {
+        resetGame();
+        gameOver = false;
+    } else {
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+            keySound.play();
+            makeGuess(event.key.toUpperCase());
+            updateDisplay();
+            checkWin();
+            checkLoss();
+        }
+    }
+};
